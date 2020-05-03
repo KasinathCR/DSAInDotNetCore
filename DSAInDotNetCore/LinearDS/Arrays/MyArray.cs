2514700bcd5b2febaf3a085fd40358ac94ab4e52
Kasinath Ravi <crkasinath@live.com>
2020-05-03 18:27:51 +0530

    DSA - Arrays - Commit 2

---
+++ DSAInDotNetCore/LinearDS/Arrays/MyArray.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace LinearDS.Arrays
+{
+    public class MyArray<T>
+    {
+        private T[] _arr;
+        private int _count;
+
+        public MyArray(int length)
+        {
+            _arr = new T[length];
+        }
+
+        public void Insert(T item)
+        {
+            if (_arr.Length == _count)
+            {
+                var newArr = new T[2 * _count];
+
+                for (var i = 0; i < _count; i++)
+                    newArr[i] = _arr[i];
+
+                _arr = newArr;
+            }
+
+            _arr[_count++] = item;
+        }
+
+        public void Print()
+        {
+            for (var i = 0; i < _count; i++)
+            {
+                Console.WriteLine(_arr[i]);
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= _count)
+                throw new ArgumentException();
+
+            for (var i = index; i < _count - 1; i++)
+            {
+                _arr[i] = _arr[i + 1];
+            }
+
+            _count--;
+        }
+
+        public int IndexOf(T item)
+        {
+            var index = -1;
+
+            for (var i = 0; i < _count; i++)
+            {
+                if (_arr[i].Equals(item))
+                {
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+}
--- DSAInDotNetCore/LinearDS/Program.cs
+++ DSAInDotNetCore/LinearDS/Program.cs
@@ -7,9 +7,26 @@ namespace LinearDS
     {
         private static void Main()
         {
+            //Extension Methods for Array Logic
+            Console.WriteLine("Extension Method for System.Array Class Approach:");
             int[] arr = { 10, 20, 30 };
             arr = (int[])arr.Insert(40);
             arr.Print();
+
+            //Simple Custom Array Class Logic
+            Console.WriteLine("Custom Array Class Approach:");
+            var myArray = new MyArray<int>(3);
+            myArray.Insert(3);
+            myArray.Insert(4);
+            myArray.Insert(5);
+            myArray.Insert(6);
+            myArray.Insert(7);
+            myArray.Print();
+            myArray.RemoveAt(3);
+            /*myArray.RemoveAt(7);*/ //Throws Exception which is expected
+            myArray.Print();
+            Console.WriteLine($"Index of Item 7 is: {myArray.IndexOf(7)}");
+
             Console.ReadKey();
         }
     }
