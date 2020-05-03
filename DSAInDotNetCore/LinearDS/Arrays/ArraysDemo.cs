7cc7ca1fdac6f198cda029872b87e4d912c652b9
Kasinath Ravi <crkasinath@live.com>
2020-05-03 16:41:16 +0530

    DSA - Arrays - Commit 1

---
+++ DSAInDotNetCore/LinearDS/Arrays/ArraysDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LinearDS.Arrays
+{
+    public static class ArraysDemo
+    {
+        public static void Print(this Array arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static Array Insert(this Array arr, int item)
+        {
+            var arrNew = new int[arr.Length + 1];
+            Array.Copy(arr, arrNew, arr.Length);
+            arrNew[^1] = item;
+            return arrNew;
+        }
+    }
+}
