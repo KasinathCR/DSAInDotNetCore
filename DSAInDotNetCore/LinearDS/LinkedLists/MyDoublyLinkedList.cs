a6965b65800f6accdca29766088f7cbb6645c503
Kasinath Ravi <crkasinath@live.com>
2020-06-13 17:30:57 +0530

    Implemented DoublyLinkedList

--- DSAInDotNetCore/LinearDS/LinkedLists/MyDoublyLinkedList.cs
+++ DSAInDotNetCore/LinearDS/LinkedLists/MyDoublyLinkedList.cs
@@ -65,6 +65,104 @@ namespace LinearDS.LinkedLists
             this.size++;
         }
 
+        public void RemoveHead()
+        {
+            if (this.IsEmpty())
+                throw new InvalidOperationException();
+            if (this._head == this._tail)
+                this._head = this._tail = null;
+            else
+            {
+                var node = this._head._next;
+                node._previous = null;
+                this._head._next = null;
+                this._head = node;
+            }
+
+            this.size--;
+        }
+
+        public void RemoveTail()
+        {
+            if (this.IsEmpty())
+                throw new InvalidOperationException();
+            if (this._head == this._tail)
+                this._head = this._tail = null;
+            else
+            {
+                var node = this._tail._previous;
+                node._next = null;
+                this._tail._previous = null;
+                this._tail = node;
+            }
+
+            this.size--;
+        }
+
+        public int Size()
+        {
+            return this.size;
+        }
+
+        public int IndexOf(T value)
+        {
+            if (this.IsEmpty())
+                throw new InvalidOperationException();
+            var node = this._head;
+            var index = 0;
+            while (node != null)
+            {
+                index++;
+                if (node._value.Equals(value))
+                    return index;
+                node = node._next;
+            }
+
+            return -1;
+        }
+
+        public T[] ToArray()
+        {
+            if (this.IsEmpty())
+                throw new InvalidOperationException();
+            var node = this._head;
+            var index = 0;
+            var arr = new T[this.size];
+            while (node != null)
+            {
+                arr[index++] = node._value;
+                node = node._next;
+            }
+
+            return arr;
+        }
+
+        public bool Contains(T value)
+        {
+            return IndexOf(value) != -1;
+        }
+
+        public void Reverse()
+        {
+            if (this.IsEmpty())
+                return;
+            var previous = this._head;
+            var current = previous._next;
+            this._head._next = null;
+            while (current != null)
+            {
+                var next = current._next;
+                previous._previous = current;
+                current._next = previous;
+                current._previous = next;
+                previous = current;
+                current = next;
+            }
+            this._tail = this._head;
+            this._head = previous;
+            this._head._previous = null;
+        }
+
         private bool IsEmpty()
         {
             return this._head == null;
